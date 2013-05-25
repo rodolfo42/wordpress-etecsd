3ddde4bc031cf53eca80da4aa7c24f65b5506ad8
Rodolfo Ferreira <rodolfo@rodolfoferreira.com.br>
2013-05-25 01:35:49 -0300

    quando nao for OpenShift, pegar config de banco de arquivo no host

--- php/wp-config.php
+++ php/wp-config.php
@@ -13,19 +13,34 @@
  *
  * @package WordPress
  */
-
+ 
+$isOpenShift = !!getenv('OPENSHIFT_APP_NAME');
+ 
+if($isOpenShift) {
+	$dbName = getenv('OPENSHIFT_APP_NAME');
+	$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+	$dbPass = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+	$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST') . ':' . getenv('OPENSHIFT_MYSQL_DB_PORT');
+} else {
+	$dbConfig = (array)json_decode(file_get_contents ('db.json'));
+	$dbName = $dbConfig['name'];
+	$dbUser = $dbConfig['user'];
+	$dbPass = $dbConfig['pass'];
+	$dbHost = $dbConfig['host'];
+}
+ 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', getenv('OPENSHIFT_APP_NAME'));
+define('DB_NAME', $dbName);
 
 /** MySQL database username */
-define('DB_USER', getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_USER', $dbUser);
 
 /** MySQL database password */
-define('DB_PASSWORD', getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_PASSWORD', $dbPass);
 
 /** MySQL hostname */
-define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST') . ':' . getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_HOST', $dbHost);
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
@@ -43,50 +58,61 @@ define('DB_COLLATE', '');
  * @since 2.6.0
  */
 
-// This is where we define the OpenShift specific secure variable functions
-require_once(ABSPATH . '../.openshift/openshift.inc');
+if($isOpenShift) {
+	// This is where we define the OpenShift specific secure variable functions
+	require_once(ABSPATH . '../.openshift/openshift.inc');
 
-// Set the default keys to use
-$_default_keys = array(
-  'AUTH_KEY'          => ' w*lE&r=t-;!|rhdx5}vlF+b=+D>a)R:nTY1Kdrw[~1,xDQS]L&PA%uyZ2:w6#ec',
-  'SECURE_AUTH_KEY'   => '}Sd%ePgS5R[KwDxdBt56(DM:0m1^4)-k6_p8}|C:[-ei:&qA)j!X`:7d-krLZM*5',
-  'LOGGED_IN_KEY'     => '$l^J?o)!zhp6s[-x^ckF}|BjU4d+(g1as)n/Q^s+k|,ZZc@E^h%Rx@VTm|0|?]6R',
-  'NONCE_KEY'         => '#f^JM8d^!sVsq]~|4flCZHdaTy.-I.f+1tc[!h?%-+]U}|_8qc K=k;]mXePl-4v',
-  'AUTH_SALT'         => 'I_wL2t!|mSw_z_ zyIY:q6{IHw:R1yTPAO^%!5,*bF5^VX`5aO4]D=mtu~6]d}K?',
-  'SECURE_AUTH_SALT'  => '&%j?6!d<3IR%L[@iz=^OH!oHRXs4W|D,VCD7w%TC.uUa`NpOH_XXpGtL$A]{+pv9',
-  'LOGGED_IN_SALT'    => 'N<mft[~OZp0&Sn#t(IK2px0{KloRcjvIJ1+]:,Ye]>tb*_aM8P&2-bU~_Z>L/n(k',
-  'NONCE_SALT'        => 'u E-DQw%[k7l8SX=fsAVT@|_U/~_CUZesq{v(=y2}#X&lTRL{uOVzw6b!]`frTQ|'
-);
+	// Set the default keys to use
+	$_default_keys = array(
+		'AUTH_KEY'          => ' w*lE&r=t-;!|rhdx5}vlF+b=+D>a)R:nTY1Kdrw[~1,xDQS]L&PA%uyZ2:w6#ec',
+		'SECURE_AUTH_KEY'   => '}Sd%ePgS5R[KwDxdBt56(DM:0m1^4)-k6_p8}|C:[-ei:&qA)j!X`:7d-krLZM*5',
+		'LOGGED_IN_KEY'     => '$l^J?o)!zhp6s[-x^ckF}|BjU4d+(g1as)n/Q^s+k|,ZZc@E^h%Rx@VTm|0|?]6R',
+		'NONCE_KEY'         => '#f^JM8d^!sVsq]~|4flCZHdaTy.-I.f+1tc[!h?%-+]U}|_8qc K=k;]mXePl-4v',
+		'AUTH_SALT'         => 'I_wL2t!|mSw_z_ zyIY:q6{IHw:R1yTPAO^%!5,*bF5^VX`5aO4]D=mtu~6]d}K?',
+		'SECURE_AUTH_SALT'  => '&%j?6!d<3IR%L[@iz=^OH!oHRXs4W|D,VCD7w%TC.uUa`NpOH_XXpGtL$A]{+pv9',
+		'LOGGED_IN_SALT'    => 'N<mft[~OZp0&Sn#t(IK2px0{KloRcjvIJ1+]:,Ye]>tb*_aM8P&2-bU~_Z>L/n(k',
+		'NONCE_SALT'        => 'u E-DQw%[k7l8SX=fsAVT@|_U/~_CUZesq{v(=y2}#X&lTRL{uOVzw6b!]`frTQ|'
+	);
 
-// This function gets called by openshift_secure and passes an array
-function make_secure_key($args) {
-  $hash = $args['hash'];
-  $key  = $args['variable'];
-  $original = $args['original'];
+	// This function gets called by openshift_secure and passes an array
+	function make_secure_key($args) {
+		$hash = $args['hash'];
+		$key  = $args['variable'];
+		$original = $args['original'];
 
-	$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  $chars .= '!@#$%^&*()';
-  $chars .= '-_ []{}<>~`+=,.;:/?|';
+		$chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+		$chars .= '!@#$%^&*()';
+		$chars .= '-_ []{}<>~`+=,.;:/?|';
 
-  // Convert the hash to an int to seed the RNG
-  srand(hexdec(substr($hash,0,8)));
-  // Create a random string the same length as the default
-  $val = '';
-  for($i = 1; $i <= strlen($original); $i++){
-    $val .= substr( $chars, rand(0,strlen($chars))-1, 1);
-  }
-  // Reset the RNG
-  srand();
-  // Set the value
-  return $val;
-}
+		// Convert the hash to an int to seed the RNG
+		srand(hexdec(substr($hash,0,8)));
+		// Create a random string the same length as the default
+		$val = '';
+		for($i = 1; $i <= strlen($original); $i++){
+			$val .= substr( $chars, rand(0,strlen($chars))-1, 1);
+		}
+		// Reset the RNG
+		srand();
+		// Set the value
+		return $val;
+	}
 
-// Generate OpenShift secure keys (or return defaults if not on OpenShift)
-$array = openshift_secure($_default_keys,'make_secure_key');
+	// Generate OpenShift secure keys (or return defaults if not on OpenShift)
+	$array = openshift_secure($_default_keys,'make_secure_key');
 
-// Loop through returned values and define them
-foreach ($array as $key => $value) {
-  define($key,$value);
+	// Loop through returned values and define them
+	foreach ($array as $key => $value) {
+		define($key,$value);
+	}
+} else {
+	define('AUTH_KEY',         '-W|kE!dYCxPDuI!TIscXl/k]dAl _)GGH4=ssk&+jk9H5AXGggd]{sG;|FxWT^ !');
+	define('SECURE_AUTH_KEY',  '0c8x>rox]C)FQpY%u5*Qj]C^RZG|]YnbwSGw3Ml<$G:-!Je0K4DW.$WQW]/8JHAk');
+	define('LOGGED_IN_KEY',    ']..QY}oC^S1WtBKaR_?cgm_b$HH+{)tJN&7`ZFlMn`AtUcOvg?V9nE-]0YxI#.#E');
+	define('NONCE_KEY',        'MwqYgh%K_<y91)?$t|o}L`(bks,+7o1HE4?6mWIRKD.&:|GB&F,UR,V)5RnA4<q>');
+	define('AUTH_SALT',        'POWP/}Qk#vb4^XC2gg60m+YX^i7;P]`?-I?[|zJi1v1o4]~a)A:.|zl5YeJ/Q1|r');
+	define('SECURE_AUTH_SALT', 'O#5N<9wyD>9;``%v@&Qh>u>z v!ON3d*1i1VIx#q=kUyL]DO%5--!D4~zD4y&kkA');
+	define('LOGGED_IN_SALT',   'a;W|Qfrf-~v=:&<J>lV#z: hjlH]+(.8tDk6gp<t-UF8&Ls_u}%`!^BW0j.{a$%-');
+	define('NONCE_SALT',       ')j3}4{G(xA/$e!{WXt(WXhc=#YuQciVw&tkW_,1jKO]3K{{<QsL-qQtuG}Hf?9V2');
 }
 
 /**#@-*/
@@ -119,7 +145,7 @@ define('WPLANG', 'pt_BR');
 define('WP_DEBUG', false);
 
 /**
- * We prefer to be secure by default
+ * We prefer to be secure by default. NOT!
  */
 define('FORCE_SSL_ADMIN', false);
 
